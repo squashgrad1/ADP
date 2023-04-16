bb2a93fb58fe42ce112dd0854b678f21ffcca794
Lisa Söte <lisa.soete@haw-hamburg.de>
2023-04-16 22:14:45 +0200

    fix Pipeline

--- Praktikumsaufgabe 1/src/NPlusEvenFilter.java
+++ Praktikumsaufgabe 1/src/NPlusEvenFilter.java
@@ -1,13 +1,11 @@
 import edu.princeton.cs.algs4.StdIn;
 
-import static edu.princeton.cs.algs4.StdIn.readInt;
-
 public class NPlusEvenFilter {
     public static void main(String[] args){
         while (!StdIn.isEmpty()) {
-            int eingabe = StdIn.readInt();
-            if (eingabe < 0 && eingabe%2 == 0){
-                System.out.print(eingabe + " ");
+            double eingabe = StdIn.readDouble();
+            if (eingabe > 0 && eingabe%2 == 0){
+                System.out.print((int) eingabe + " ");
             }
         }
 
--- Praktikumsaufgabe 1/src/TestPipelineing.java
+++ Praktikumsaufgabe 1/src/TestPipelineing.java
@@ -1,9 +1,11 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class TestPipelineing {
     public static void main (String[] args){
 
         int zaehler = Integer.parseInt(args[0]);
         int nenner = Integer.parseInt(args[1]);
-       // StdOut.print((double) zaehler/nenner);
-        System.out.println((double) zaehler/nenner);
+        StdOut.print((double) zaehler/nenner);
+        //System.out.println((double) zaehler/nenner);
     }
 }
